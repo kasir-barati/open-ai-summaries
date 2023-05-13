7d50a38aefe682d409259e009b4ff6c7d083389e
Kasir Barati <kasir.barati@gmail.com>
2023-05-13 20:36:07 +0200

    chore: upgrade 3rd party libs

--- apps/chrome/vite.config.ts
+++ apps/chrome/vite.config.ts
@@ -5,22 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
-    // define: {
-    //     'process.env': {},
-    // },
-    // build: {
-    //     emptyOutDir: true,
-    //     outDir: resolve(__dirname, 'dist'),
-    //     lib: {
-    //         formats: ['iife'],
-    //         entry: resolve(__dirname, './src/main.tsx'),
-    //         name: 'Open AI Summaries',
-    //     },
-    //     rollupOptions: {
-    //         output: {
-    //             entryFileNames: 'index.js',
-    //             extend: true,
-    //         },
-    //     },
-    // },
 });
